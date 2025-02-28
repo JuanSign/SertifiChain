cd105492957a4cb8e3829d80e8d19e70c9139698
Juan Siagian <jsa.siagian@gmail.com>
2025-02-28 20:35:21 +0700

    fix : canister code bug

--- sertifichain/src/sertifichain_backend/src/lib.rs
+++ sertifichain/src/sertifichain_backend/src/lib.rs
@@ -18,30 +18,31 @@ thread_local! {
 
 #[update]
 fn issue_certificate(id: CertificateId, owner: String, location: String, size: f64, issued_by: String) -> String {
-    let mut certs: HashMap<CertificateId, LandCertificate> = storage::stable_restore().unwrap_or_default();
-    if certs.contains_key(&id) {
-        return format!("Certificate {} already exists!", id);
-    }
-    let cert = LandCertificate {
-        owner: owner.clone(),
-        location,
-        size,
-        issued_by,
-    };
-    certs.insert(id, cert);
-    storage::stable_save((certs,)).unwrap();
-    format!("Certificate {} issued to {}", id, owner)
+    CERTIFICATES.with(|certs| {
+        let mut certs = certs.borrow_mut();
+        if certs.contains_key(&id) {
+            return format!("Certificate {} already exists!", id);
+        }
+        let cert = LandCertificate {
+            owner: owner.clone(),
+            location,
+            size,
+            issued_by,
+        };
+        certs.insert(id, cert);
+        format!("Certificate {} issued to {}", id, owner)
+    })
 }
 
 #[update]
-fn transfer_ownership(id: CertificateId, new_owner: String) -> Result<String, String> {
+fn transfer_ownership(id: CertificateId, new_owner: String) -> (bool, String) {
     CERTIFICATES.with(|certs| {
         let mut certs = certs.borrow_mut();
         if let Some(cert) = certs.get_mut(&id) {
             cert.owner = new_owner.clone();
-            return Ok(format!("Certificate {} ownership transferred to {}", id, new_owner));
+            return (true, format!("Certificate {} ownership transferred to {}", id, new_owner));
         }
-        Err("Certificate not found".to_string())
+        (false, "Certificate not found".to_string())
     })
 }
 
