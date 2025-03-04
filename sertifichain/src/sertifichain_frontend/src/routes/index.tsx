c3ab0acd6eb6167cca526d56269fa4f54f26ded2
rifchzschki <rifkiqizara23@gmail.com>
2025-03-04 13:06:51 +0700

    feat: init tailwind config

--- sertifichain/src/sertifichain_frontend/src/routes/index.tsx
+++ sertifichain/src/sertifichain_frontend/src/routes/index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
+    <div className="bg-primary text-primary-foreground p-md rounded-md shadow-md">
       <h3>Welcome Home!</h3>
     </div>
   )
